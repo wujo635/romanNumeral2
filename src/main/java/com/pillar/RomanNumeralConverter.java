564a5022af701194c65737617253a1d7fe0fc3d7
wujo <wujo635@gmail.com>
2019-02-26 01:12:59 -0500

    Pass convert V to 5

--- src/main/java/com/pillar/RomanNumeralConverter.java
+++ src/main/java/com/pillar/RomanNumeralConverter.java
@@ -2,7 +2,16 @@ package com.pillar;
 
 public class RomanNumeralConverter {
 
+    private static int[] arabicValues = new int[]{1,5};
+    private static String[] romanNumerals = new String[]{"I","V"};
+
     public int convert(String romanNumeral) {
-        return 1;
+        int value = -1;
+        for (int index = 0; index < romanNumerals.length; index++) {
+            if (romanNumeral.equals(romanNumerals[index])) {
+                return arabicValues[index];
+            }
+        }
+        return value;
     }
 }
