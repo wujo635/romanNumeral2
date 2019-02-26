5bd0cb174979d986bc46855573509aa052065237
wujo <wujo635@gmail.com>
2019-02-26 01:14:45 -0500

    Fail converting single roman numeral to arabic equivalent

--- src/test/java/com/pillar/RomanNumeralConverterTest.java
+++ src/test/java/com/pillar/RomanNumeralConverterTest.java
@@ -18,4 +18,15 @@ public class RomanNumeralConverterTest {
         assertEquals(5, converter.convert("V"));
     }
 
+    @Test
+    public void shouldConvertSingleRomanNumeralsToArabicEquivalent() {
+        assertEquals(1, converter.convert("I"));
+        assertEquals(5, converter.convert("V"));
+        assertEquals(10,converter.convert("X"));
+        assertEquals(50,converter.convert("L"));
+        assertEquals(100,converter.convert("C"));
+        assertEquals(500,converter.convert("D"));
+        assertEquals(1000,converter.convert("M"));
+    }
+
 }
