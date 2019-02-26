4a58c31dba1396a1955e5a1642885056a4c9b2dd
wujo <wujo635@gmail.com>
2019-02-26 01:17:42 -0500

    Refactor method name to be more specific

--- src/main/java/com/pillar/RomanNumeralConverter.java
+++ src/main/java/com/pillar/RomanNumeralConverter.java
@@ -5,7 +5,7 @@ public class RomanNumeralConverter {
     private static int[] arabicValues = new int[]{1, 5, 10, 50, 100, 500, 1000};
     private static String[] romanNumerals = new String[]{"I", "V", "X", "L", "C", "D", "M"};
 
-    public int convert(String romanNumeral) {
+    public int convertRomanToArabic(String romanNumeral) {
         int value = -1;
         for (int index = 0; index < romanNumerals.length; index++) {
             if (romanNumeral.equals(romanNumerals[index])) {
--- src/test/java/com/pillar/RomanNumeralConverterTest.java
+++ src/test/java/com/pillar/RomanNumeralConverterTest.java
@@ -10,13 +10,13 @@ public class RomanNumeralConverterTest {
 
     @Test
     public void shouldConvertSingleRomanNumeralsToArabicEquivalent() {
-        assertEquals(1, converter.convert("I"));
-        assertEquals(5, converter.convert("V"));
-        assertEquals(10,converter.convert("X"));
-        assertEquals(50,converter.convert("L"));
-        assertEquals(100,converter.convert("C"));
-        assertEquals(500,converter.convert("D"));
-        assertEquals(1000,converter.convert("M"));
+        assertEquals(1, converter.convertRomanToArabic("I"));
+        assertEquals(5, converter.convertRomanToArabic("V"));
+        assertEquals(10,converter.convertRomanToArabic("X"));
+        assertEquals(50,converter.convertRomanToArabic("L"));
+        assertEquals(100,converter.convertRomanToArabic("C"));
+        assertEquals(500,converter.convertRomanToArabic("D"));
+        assertEquals(1000,converter.convertRomanToArabic("M"));
     }
 
 }
